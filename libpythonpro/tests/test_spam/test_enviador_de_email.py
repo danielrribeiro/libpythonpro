3613ad2f7095aed72534596a738438edaf8e6ab2
@danielrribeiro <danielrodriguesr@gmail.com>
2022-10-10 11:16:20 -0300

    Começado o Desenvolvimento Orientado a testes (TDD) a partir dos Baby Steps

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,38 @@
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['foo@bar.com.br', 'danielrodriguesr@gmail.com']
+)
+def test_remetente(destinatario):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        destinatario,
+        'luciano@python.pro.br',
+        'Cursos Python Pro',
+        'Primeira turma Guido Von Rossum aberta.'
+    )
+    assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['', 'danielrodriguesr']
+)
+def test_remetente_invalido(destinatario):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            destinatario,
+            'jgrsilva55@gmail.com',
+            'Cursos Python Pro',
+            'Primeira turma Guido Von Rossum aberta.'
+        )
