bcb5c2aa6719844d3f8cb803a6ffc8dbf84f8b00
@danielrribeiro <danielrodriguesr@gmail.com>
2022-10-13 12:14:27 -0300

    Realizada Injeção de Dependências

--- libpythonpro/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,10 +1,59 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadordeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadordeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Daniel', email='danielrodriguesr@gmail.com'),
+            Usuario(nome='Geraldo', email='jgrsilva55@gmail.com')
+        ],
+        [
+            Usuario(nome='Daniel', email='danielrodriguesr@gmail.com'),
+        ]
+    ]
+)
+def test_qtd_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadordeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
+        'danielrodriguesr@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+    assert len(usuarios) == enviador.qtd_email_enviados
+
+
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Daniel', email='danielrodriguesr@gmail.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadordeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'jgrsilva55@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+    assert enviador.parametros_de_envio == (
+        'jgrsilva55@gmail.com',
         'danielrodriguesr@gmail.com',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
